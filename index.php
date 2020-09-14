04d6f3f0a1d0ce4057efd4743ee1166c5f4ec06c
YeMyatThu11 <scm.yemyatthu@gmail.com>
2020-09-14 14:07:34 +0630

    ConnectCSSandJSfiles

--- index.php
+++ index.php
@@ -1,7 +1,15 @@
 <html>
 <head>
-    <title>Tutorial theme</title>
+<title>Tutorial theme</title>
+<script type="text/javascript" src="<?php echo get_stylesheet_directory_uri().'/js/jquery.min.js'; ?>">
+</script>
+<script type="text/javascript" src="<?php echo get_stylesheet_directory_uri().'/js/bootstrap.js'; ?>">
+</script>
+<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri().'/css/bootstrap.min.css'; ?>">
+<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
 </head>
+<body>
     
 </body>
+
 </html>
